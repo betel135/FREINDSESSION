1a1bd2f80805389b7ce846e20fe4ef63f85b036c
betel135 <68611810+betel135@users.noreply.github.com>
2021-03-24 21:02:18 -0400

    first commit

---
+++ src/Home/Header/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+class LogIn extends React.Component{
+    render(){
+        return(
+            <div>
+<h1>This is Log In</h1>
+            </div>
+        )
+    }
+}
+export default LogIn
